e76c2b70a23e89aad0ee68b86e22ba59117b6a2e
rockdr15 <rodindaniel123@gmail.com>
2018-12-20 20:15:05 -0500

    exe

---
+++ exercise2.rb
@@ -0,0 +1,9 @@
+puts 55/18.0 # It will give u a long decimal round off to nearest dollar amount 
+
+apple = 3 # have to give the string a value 1st
+
+puts 3 + apple 
+
+puts "I wish I had #{45628 * 7839} dollars in my bank account."
+
+(10 < 20 && 30 < 20) || !(10 == 11)
---
+++ exercise3.rb
@@ -0,0 +1,16 @@
+puts "what is your name?"
+
+user_name = gets.chomp
+
+puts "Hello, #{user_name}"
+
+puts "How old are you?"
+
+age = gets.chomp
+
+puts "cool your #{age}"
+
+
+if my_number > 1
+    puts "The number is greater then 1"
+end
